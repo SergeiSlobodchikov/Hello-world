7ba91507f4337f0f7700eeb8e7a8b3b834f194b7
Sergei Slobodchikov <cerhclobodhikow@mail.ru>
2022-10-12 10:03:24 +0300

    home work dog

---
+++ Home_work/Program.cs
@@ -0,0 +1,26 @@
+﻿int count = 0;
+double distance = 10000;
+double firstfriendspeed = 1;
+double secondfriendspeed = 2;
+double dogspeed = 5;
+int friend = 2;
+double time = 0;
+
+while(distance > 10)
+{
+    while(friend==1)
+    {
+        time = (distance/(firstfriendspeed+dogspeed));
+        friend = 2;
+    }
+    while(friend==2)
+    {
+        time = (distance/(secondfriendspeed+dogspeed));
+        friend = 1;
+    }
+    distance = distance - (firstfriendspeed + secondfriendspeed) * time;
+    count = count + 1;
+}
+
+Console.WriteLine("Количество раз пробежала собака ");
+Console.Write(count);
