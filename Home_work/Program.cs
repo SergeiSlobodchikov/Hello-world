bb2f4e4ba94037c4df5b56eb17565ef620d4c1c9
Sergei Slobodchikov <cerhclobodhikow@mail.ru>
2022-10-12 17:27:33 +0300

    доработка 3

--- Home_work/Program.cs
+++ Home_work/Program.cs
@@ -1,11 +1,15 @@
-﻿int count = 0;
-double distance = 10000;
-double firstfriendspeed = 1;
-double secondfriendspeed = 2;
-double dogspeed = 5;
-double friend = 2;
+﻿Console.Clear();
+int count = 0;
+Console.WriteLine("Введите расстояние (м)");
+double distance = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите скорость 1-го друга (м/с)"); 
+double firstfriendspeed = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Введите скорость 2-го друга (м/с)"); 
+double secondfriendspeed = Convert.ToInt32(Console.ReadLine()); 
+Console.WriteLine("Введите скорость собаки (м/с)"); 
+double dogspeed = Convert.ToInt32(Console.ReadLine()); 
+int friend = 2;
 double time = 0;
-
 while(distance>10)
 {
     if(friend==1)
@@ -21,5 +25,4 @@ while(distance>10)
     distance = distance - (firstfriendspeed + secondfriendspeed) * time;
     count = count + 1;
 }
-
 Console.WriteLine("Собака пробежит " + count + " раз");
