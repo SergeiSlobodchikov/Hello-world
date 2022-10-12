78b6640e873e8e5e84d251677fd56968ad80fa00
Sergei Slobodchikov <cerhclobodhikow@mail.ru>
2022-10-12 17:06:23 +0300

    доработка

--- Home_work/Program.cs
+++ Home_work/Program.cs
@@ -3,17 +3,17 @@ double distance = 10000;
 double firstfriendspeed = 1;
 double secondfriendspeed = 2;
 double dogspeed = 5;
-int friend = 2;
+double friend = 2;
 double time = 0;
 
-while(distance > 10)
+while(distance>10)
 {
     if(friend==1)
     {
         time = (distance/(firstfriendspeed+dogspeed));
         friend = 2;
     }
-    if(friend==2)
+    else
     {
         time = (distance/(secondfriendspeed+dogspeed));
         friend = 1;
