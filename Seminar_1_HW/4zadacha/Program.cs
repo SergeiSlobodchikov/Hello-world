8f0dbcb3f5c4134df7fe22bc8ac0e6315a684e6b
Sergei Slobodchikov <cerhclobodhikow@mail.ru>
2022-10-13 17:06:15 +0300

    4 zadacha

---
+++ Seminar_1_HW/4zadacha/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число A");
+double A = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число B");
+double B = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число C");
+double C = Convert.ToDouble(Console.ReadLine());
+double max1 = Math.Max(A, B);
+Console.WriteLine("Максимальное число из трех чисел " +  Math.Max(max1, C));
