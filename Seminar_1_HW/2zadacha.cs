6149ad3fcfd336e9cd829561c0ac35df5eeaf407
Sergei Slobodchikov <cerhclobodhikow@mail.ru>
2022-10-13 16:59:24 +0300

    2zadacha

---
+++ Seminar_1_HW/2zadacha.cs
@@ -0,0 +1,6 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число A");
+double A = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите число B");
+double B = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Максимальное число из двух чисел " +  Math.Max(A, B));
