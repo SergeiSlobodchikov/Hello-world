c7484a16a47eed69cc759d6964a0c90ac37ca87d
Sergei Slobodchikov <cerhclobodhikow@mail.ru>
2022-10-13 17:19:06 +0300

    6 zadacha

---
+++ Seminar_1_HW/zadacha6/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Clear();
+Console.WriteLine("Введите число для проверки четное оно или нет");
+int x = Convert.ToInt32(Console.ReadLine());
+        if ((x % 2) == 0)
+        {
+            Console.WriteLine("четное");
+        }
+        else
+        {
+            Console.WriteLine("нечетное");
+        }
