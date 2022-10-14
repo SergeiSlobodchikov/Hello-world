41ac2d9785ee28968942f222f49edd867072bf27
Sergei Slobodchikov <cerhclobodhikow@mail.ru>
2022-10-14 10:30:51 +0300

    zadachi is seminara2

--- Seminar/seminar007/Program.cs
+++ Seminar/seminar007/Program.cs
@@ -2,51 +2,46 @@
 Console.Clear();
 Console.Write("Введите число от 1 до 7: ");
 int i = Math.Abs((Convert.ToInt32(Console.ReadLine())));
-while (i > 7 ^ i < 1)
+while (i > 0)
+{
+if (i == 1)
+{
+    Console.Write("Понедельник");
+    break;
+}
+else if (i == 2)
+{
+    Console.Write("Вторник");
+    break;
+}
+else if (i == 3)
+{
+    Console.Write("Среда");
+    break;
+}
+else if (i == 4)
+{
+    Console.Write("Четверг");
+    break;
+}
+else if (i == 5)
+{
+    Console.Write("Пятница");
+    break;
+}
+else if (i == 6)
+{
+    Console.Write("Суббота");
+    break;
+}
+else if (i == 7)
+{
+    Console.Write("Воскресенье");
+    break;
+}
+else
 {
     Console.Write("Не то число");
     break;
 }
-while (i >= 1 && i <= 7)
-{
-    if (i == 1)
-    {
-        Console.Write("Понедельник");
-        break;
-    }
-    if (i == 2)
-    {
-        Console.Write("Вторник");
-        break;
-    }
-    if (i == 3)
-    {
-        Console.Write("Среда");
-        break;
-    }
-    if (i == 4)
-    {
-        Console.Write("Четверг");
-        break;
-    }
-    if (i == 5)
-    {
-        Console.Write("Пятница");
-        break;
-    }
-    if (i == 6)
-    {
-        Console.Write("Суббота");
-        break;
-    }
-    if (i == 7)
-    {
-        Console.Write("Воскресенье");
-        break;
-    }
-    else
-    {
-        Console.Write("Не то число");
-        break;
-    }
 }
