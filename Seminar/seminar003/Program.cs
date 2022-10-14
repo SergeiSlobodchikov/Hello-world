0be17319e1714576fa3164561aec9a35609e409d
Sergei Slobodchikov <cerhclobodhikow@mail.ru>
2022-10-14 09:00:09 +0300

    SeminarZadacha3

---
+++ Seminar/seminar003/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите трехзначное число: ");
+int num = Math.Abs((Convert.ToInt32(Console.ReadLine())));
+if (num > 99 && num <= 999)
+{
+    Console.Write("Последняя цифра: " + num % 10);
+}
+else
+{
+    Console.WriteLine("Введенное число не трехзначное");
+}
